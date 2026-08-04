204d8cbf066cfaf802d23b3aaac40c6fda2632a7
khadilkaryash769 <khadilkaryash769@gmail.com>
2026-08-04 19:32:32 +0530

    Version 12 - Enterprise Logging

--- Scripts/spark_extract.py
+++ Scripts/spark_extract.py
@@ -1,10 +1,20 @@
-from pyspark.sql import SparkSession
-
+from spark_logger import logger
 
 def extract_data(spark):
-    df = spark.read.csv(
-        "data/Raw/customers.csv",
-        header=True,
-        inferSchema=True
+
+    file_path = "data/Raw/customers.csv"
+
+    logger.info(f"Reading file : {file_path}")
+
+    df = (
+        spark.read
+        .option("header", True)
+        .option("inferSchema", True)
+        .csv(file_path)
     )
+
+    print(f"Extracted Records : {df.count()}")
+
+    logging.info(f"Extracted {df.count()} records")
+
     return df
